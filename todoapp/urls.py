da75618c1879f26c55b058c72d2283e2eb7e3bb4
amol221 <patilamol1045@gmail.com>
2025-08-16 16:27:38 +0530

    todo list app complete[C

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -2,5 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('addTask/',views.addTask , name='addTask')
+    path('addTask/',views.addTask , name='addTask'),
+    path('mark_as_done/<int:pk>/', views.mark_as_done,name = 'mark_as_done'),
+    path('mark_as_undone/<int:pk>/', views.mark_as_undone,name = 'mark_as_undone'),
+
+    # DELETE
+    path('delete_tasks/<int:pk>/',views.delete_tasks,name='delete_tasks'),
+
+    #EDIT
+    path('update_tasks/<int:pk>/',views.update_tasks,name='update_tasks'),
+
 ]
--- todoproject/views.py
+++ todoproject/views.py
@@ -5,7 +5,10 @@ from todoapp.models import Task
 
 def home(request):
     tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')   # '-updated_at' give data in desc order and 'updated_at' will give in asce ordr 
+    completed_tasks = Task.objects.filter(is_completed = True)
     context = {
-        'tasks': tasks
+        'tasks': tasks,
+        'completed_tasks':completed_tasks
     }
     return render(request,'home.html',context)
+
