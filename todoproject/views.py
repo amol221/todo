3fa44c3758fbf6a4f2bc0f18cfddc4d6426e2dd2
amol221 <patilamol1045@gmail.com>
2025-08-16 15:12:06 +0530

    add post functionality

--- todoproject/views.py
+++ todoproject/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from todoapp.models import Task
 
 
 def home(request):
-    return render(request,'home.html')
+    tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')   # '-updated_at' give data in desc order and 'updated_at' will give in asce ordr 
+    context = {
+        'tasks': tasks
+    }
+    return render(request,'home.html',context)
